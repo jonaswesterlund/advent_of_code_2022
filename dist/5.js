2c8bae89a49547a8dbd2c6f162aa05b69c66bf48
Jonas Westerlund <jh.westerlund@gmail.com>
2022-12-09 13:22:55 +0200

    Day 5 complete

--- dist/5.js
+++ dist/5.js
@@ -1,26 +1,55 @@
 import { readFile } from "./helpers.js";
-const input = readFile("5_test");
-const numberOfStacks = 3;
-const highestHeight = 3;
+const input = readFile("5");
+const numberOfStacks = 9;
+const highestHeight = 8;
 let height = 0;
 let stackArray = new Array(numberOfStacks);
+let moves = [];
 for (let i = 0; i < numberOfStacks; i++) {
     stackArray[i] = [];
 }
 for (let line of input.split("\n")) {
-    if (height >= highestHeight) {
-        break;
-    }
-    const lineChars = line.split("");
-    for (let i = 0; i < numberOfStacks; i++) {
-        if (lineChars[4 * i + 1] !== " ") {
-            stackArray[i].push(lineChars[4 * i + 1]);
+    if (height < highestHeight) {
+        const lineChars = line.split("");
+        for (let i = 0; i < numberOfStacks; i++) {
+            if (lineChars[4 * i + 1] !== " ") {
+                stackArray[i].push(lineChars[4 * i + 1]);
+            }
         }
+        height++;
     }
-    height++;
+    else if (line.includes("move")) {
+        const moveCommands = line.split(" ");
+        const amount = Number(moveCommands[1]);
+        const from = Number(moveCommands[3]);
+        const to = Number(moveCommands[5]);
+        moves.push({ amount, from, to });
+    }
 }
 for (let i = 0; i < numberOfStacks; i++) {
     stackArray[i] = stackArray[i].reverse();
 }
-console.log(stackArray);
+function executeSingleMoves(moves, stackArray) {
+    for (let move of moves) {
+        for (let i = 0; i < move.amount; i++) {
+            const crate = stackArray[move.from - 1].pop();
+            stackArray[move.to - 1].push(crate);
+        }
+    }
+}
+function executeMultipleMoves(moves, stackArray) {
+    for (let move of moves) {
+        let fromStack = stackArray[move.from - 1];
+        let toStack = stackArray[move.to - 1];
+        fromStack.slice(fromStack.length - move.amount).forEach(c => toStack.push(c));
+        for (let i = 0; i < move.amount; i++) {
+            fromStack.pop();
+        }
+    }
+}
+function getTopCrates(stackArray) {
+    return stackArray.reduce((topCrates, stack) => `${topCrates}${stack[stack.length - 1]}`, "");
+}
+executeMultipleMoves(moves, stackArray);
+console.log(getTopCrates(stackArray));
 //# sourceMappingURL=5.js.map
--- src/5.ts
+++ src/5.ts
@@ -1,11 +1,12 @@
 import { readFile } from "./helpers.js";
 
-const input = readFile("5_test");
-const numberOfStacks = 3;
-const highestHeight = 3;
+const input = readFile("5");
+const numberOfStacks = 9;
+const highestHeight = 8;
 
 let height = 0;
 let stackArray = new Array(numberOfStacks)
+let moves = [];
 for(let i = 0; i < numberOfStacks; i++) {
     stackArray[i] = [];
 }
@@ -19,11 +20,41 @@ for (let line of input.split("\n")) {
       }
     }
     height++
-  } else {
-    console
+  } else if (line.includes("move")) {
+    const moveCommands = line.split(" ");
+    const amount = Number(moveCommands[1]);
+    const from = Number(moveCommands[3]);
+    const to = Number(moveCommands[5]);
+    moves.push({amount, from, to});    
   }
-  
 }
 for(let i = 0; i < numberOfStacks; i++) {
     stackArray[i] = stackArray[i].reverse();
 }
+
+function executeSingleMoves(moves: any, stackArray: any) {
+  for(let move of moves) {
+    for(let i = 0; i < move.amount;i++) {
+      const crate = stackArray[move.from-1].pop();
+      stackArray[move.to-1].push(crate)
+    }
+  }
+}
+
+function executeMultipleMoves(moves: any, stackArray: any) {
+  for(let move of moves) {
+    let fromStack: string[] = stackArray[move.from-1];
+    let toStack: string[] = stackArray[move.to-1];
+    fromStack.slice(fromStack.length-move.amount).forEach(c => toStack.push(c));
+    for(let i = 0; i < move.amount;i++) {
+      fromStack.pop();
+    }
+  }
+}
+
+
+function getTopCrates(stackArray: any) {
+  return stackArray.reduce((topCrates, stack) => `${topCrates}${stack[stack.length-1]}`,"")
+}
+executeMultipleMoves(moves, stackArray);
+console.log(getTopCrates(stackArray));
